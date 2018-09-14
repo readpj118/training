7230414faa602d3ca138ba2c45db57d49ab0ffda
Phil Read <phil.read@kgb.com>
2018-09-14 11:47:33 +0100

    failed scenario screen shots gitignore file checkboxes feature

---
+++ features/step_definitions/the_internet/Pages/check_boxes_page.rb
@@ -0,0 +1,17 @@
+class CheckBoxesPage
+  include Capybara::DSL
+  attr_reader :checkbox_one_xpath, :checkbox_two_xpath
+
+  def initialize
+    @checkbox_one_xpath = '//*[@id="checkboxes"]/input[1]'
+    @checkbox_two_xpath = '//*[@id="checkboxes"]/input[2]'
+  end
+
+  def click_checkbox_one
+    page.find(:xpath, @checkbox_one_xpath).click
+  end
+
+  def click_checkbox_two
+    page.find(:xpath, @checkbox_two_xpath).click
+  end
+end
